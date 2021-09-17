394807c1e504dc936ee3525b7f7698c388a9bf05
Yang, GyeongPil <ykp9711@gmail.com>
2021-09-18 07:40:36 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,19 +1,11 @@
 package git_test2;
 
 public class Hello {
+    
+    String message = "æ»≥Á«œººø‰";
 
-    String message;
-
-    public Hello() {
-        this("æ»≥Á«œººø‰");
-    }
-
-    public Hello(String message) {
-        this.message = message;
-    }
-
-    public void say() {
-        System.out.println(message);
+    public String say() {
+        return message;
     }
 
 }
